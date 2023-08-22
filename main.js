8c91df4b52c7ccf049853b36b1c80a94cbbb6ceb
Ratnesh433 <35849041+Ratnesh433@users.noreply.github.com>
2023-08-22 20:42:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -21,11 +21,7 @@ let peerConnection;
 const servers = {
     iceServers:[
         {
-            urls:['stun.l.google.com:19302',
-                    'stun1.l.google.com:19302',
-                    'stun2.l.google.com:19302',
-                    'stun3.l.google.com:19302','stun4.l.google.com:19302'
-         ]
+            urls:['stun.l.google.com:19302'  ]
         }
     ]
 }
